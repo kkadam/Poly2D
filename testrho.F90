2df8b1317435e60466135d67b0062645f0b7bb59
Kundan Kadam <kkadam1@tigers.lsu.edu>
2013-08-29 14:23:26 -0500

    testrho works great!

--- testrho.F90
+++ testrho.F90
@@ -19,6 +19,10 @@ subroutine testrho(id,gh,den)
 !gh=1 => Adds a ghost structure to the density
 !den = The constant value of density
 	
+  print*, ">>>Testrho"
+  
+  rho=0.0
+  
   if (id==1) then
     call sph(den,m,vol,mom)
   elseif (id==2) then
@@ -33,7 +37,7 @@ subroutine testrho(id,gh,den)
   
   
   call print2d(rho,"test2d")
-  call print1d(rho,"y",1,"tes1d")
+  call print1d(rho,"y",2,"tes1d")
   print*, "Total mass=", m, "Total volume=",vol,"MI=",mom
   
 end subroutine testrho
@@ -50,30 +54,44 @@ subroutine sph(den,m,vol,mom)
   real, dimension(numr,numz,numphi) :: pot, rho
   common /poisson/ pot, rho
   
-  integer:: i,j,k
+  integer:: i,j,k,count
   real :: radius, m, vol, mom,angle,den,r,c,pi
   
   Pi=3.14159265359
   radius=(ax-1.5)	
   c=0
+  count=0
+  
+  rho=0.0
   
   do i=1,numr
     do j=1,numz
         r=((i-1.5)**2+(j+c-1.5)**2)**0.5
-        if (r**2.le.radius**2) then
-          rho(i,j,1)=den   
+        if ((r**2.le.radius**2).and.(j.ge.2).and.(i.ge.2)) then
+          rho(i,j,1)=den      
+          count=count+1
+          
+          
+          
         else
           rho(i,j,1)=0.0
         endif
+        
+                  
+      !    if (j==82) then
+      !      print*,rho(i,j,1), i,j
+      !    endif
+        
+        
+        
     enddo
   enddo
   
-  radius=(ax-1.5)/(numr-1)
-  
+  radius=(ax-1.5-1)/(numr-1)
   vol=4.0/3*pi*radius**3
   m=den*vol
   mom=2.0/5*m*radius**2
-  
+       print*, "anacount", count
 end subroutine sph
 
 
@@ -84,29 +102,32 @@ subroutine cyl(den,m,vol,mom)
   real, dimension(numr,numz,numphi) :: pot, rho
   common /poisson/ pot, rho
   
-  integer:: i,j,k
+  integer:: i,j,k, count
   real :: radius, height, m, vol, mom,den, pi
 
-  
+  count=0
   Pi=3.14159265359
   
-  do i=1,numr
-    do j=1,numz
+  do i=2,numr
+    do j=2,numz
       if ((i.le.ax).and.(j.le.by)) then
         rho(i,j,1)=den
+        
+        count=count+1
+        
       else
         rho(i,j,1)=0.0
       endif
     enddo
   enddo
   
-  radius=(ax-1.5)/(numr-1)
-  height=(by-1.5)/(numr-1)
+  radius=(ax-1.5-1)/(numr-1)
+  height=(by-1.5-1)/(numr-1)
   
   vol=pi*radius**2*height*2
   m=den*vol
   mom=(m*radius**2)/2.0  
-  
+  print*, "anacount", count
 end subroutine cyl
 
 
@@ -118,7 +139,7 @@ subroutine lin(den,m,vol,mom)
   common /poisson/ pot, rho
 
   integer:: i,j,k
-  real :: slope, c, pi
+  real :: slope, c, pi, count
   real :: radius, height, m, vol, mom, den
   
   Pi=3.14159265359
@@ -126,14 +147,17 @@ subroutine lin(den,m,vol,mom)
   slope=-by*1.0/ax	
   c=by*1.0  
   
+  
+  count=0
   radius=(ax-1.5)
   Pi=3.14159265359
   
-  do i=1,numr
-    do j=1,numz
+  do i=2,numr
+    do j=2,numz
       do k=1,numphi
         if (j .le. slope * i + c) then
-          rho(i,j,k)=den       
+          rho(i,j,k)=den     
+          count=count+1
         else
           rho(i,j,k)=0.0
         endif
@@ -141,13 +165,13 @@ subroutine lin(den,m,vol,mom)
     enddo
   enddo
   
-  radius=(ax-1.5)/(numr-1)
-  height=(by-1.5)/(numr-1)
+  radius=(ax-1.5-1)/(numr-1)
+  height=(by-1.5-1)/(numr-1)
   
   vol=pi/3.0*radius**2*height*2
   m=den*vol
   mom=0.3*(m*radius**2)  !Not *2 because vol takes care of it  
-  
+  print*, "anacount", count
   
 end subroutine lin
 
